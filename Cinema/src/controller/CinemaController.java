516f0eeb6b7e94981b36e1fb9a8f710900e47e68
lukas raphael <lukas.rmdl@gmail.com>
2022-12-25 18:37:37 -0300

    Modelagem das classes

---
+++ Cinema/src/controller/CinemaController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import model.*;
+
+import java.util.GregorianCalendar;
+
+public class CinemaController {
+    public static void main(String[] args){
+        //Instanciando Classe de Ator
+        Ator ator1 = new Ator("James Cameron");
+
+        //Instanciando Classe de Genero
+        Genero genero1 = new Genero("ficção cientifica");
+
+        //Instanciando Classe de Atua
+        Atua atua1 = new Atua("Navih", ator1);
+
+        //Instanciando Classe de Filme
+        Filme filme1 = new Filme("Avatar",genero1, atua1);
+
+        //Intanciando classe de assento
+        Assento assento1 = new Assento("1A");
+
+        //Intanciando classe de sala
+        Sala sala1 = new Sala(30, 1);
+
+        //Intanciando classe de Sessão
+        Sessao sessao1 = new Sessao(filme1, sala1,new GregorianCalendar(2021, 5, 8, 10, 01), 12.20,5.10,Boolean.FALSE);
+
+        //Instanciando Ingresso
+        Ingresso ingresso1 = new Ingresso("A1", 2, sessao1);
+
+        System.out.println(ingresso1);
+
+
+
+    }
+}
---
+++ Cinema/src/model/Ator.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ator {
+    private String nomeAtor;
+
+    private List<Atua> atuou = new ArrayList<>();
+
+
+    //Construtores
+
+
+    public Ator(){
+        super();
+    }
+
+    public Ator(String nomeAtor){
+        super();
+        this.nomeAtor = nomeAtor;
+    }
+
+    public Ator(String nomeAtor, List<Atua> atuou){
+        super();
+        this.nomeAtor = nomeAtor;
+        this.atuou = atuou;
+    }
+
+    //Getters
+
+    public String getNomeAtor() {
+        return nomeAtor;
+    }
+
+    //Setters
+
+    public void setNomeAtor(String nomeAtor) {
+        this.nomeAtor = nomeAtor;
+    }
+
+    @Override
+    public String toString() {
+        return "ator{" +
+                "ator=" + nomeAtor +
+                '}';
+    }
+}
